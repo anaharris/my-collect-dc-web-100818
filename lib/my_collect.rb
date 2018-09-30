ad43457efa0054e247e58e01c568e5eacb794c47
Ana Harris <vana1990@gmail.com>
2018-09-30 19:07:29 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  new_array = []
+  i = 0 
+  while i < array.size
+    array[i]
+    new_elements = yield(array)
+    new_array << new_elements
+    i+=1
+  end
+  new_array
+end
